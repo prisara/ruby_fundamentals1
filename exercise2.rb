448eb0997b2cf3c9367bb1366b3eab05c0cf9646
prisara <prisaramorale@gmail.com>
2016-10-23 13:41:59 -0400

    Commit exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+# 1. integers and floats
+puts 55 * 0.20
+
+# 2. convert integer into string
+puts "number" + 9.to_s
+
+# 3. string interpolation
+puts "45628 multiplied by 7839 is #{45628 * 7839}"
+
+# 4. boolean expression
+puts (10 < 20 && 30 < 20) || !(10 == 11) # true
